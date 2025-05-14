6aadc7d07323094a00f70fdd1ebc06172812951e
Kseniya Bristol <126690376+KsBristol@users.noreply.github.com>
2025-05-14 14:32:55 +0300

    Update ball.py
    add score

--- ball.py
+++ ball.py
@@ -3,14 +3,15 @@ import pygame
 
 class Ball(pygame.sprite.Sprite):
     """ Для обработки групп спрайтов"""
-    def __init__(self, x, speed, surf, group):
+    def __init__(self, x, speed, surf, score, group):
         pygame.sprite.Sprite.__init__(self)
         # изображение спрайта(ссылка на передаваемую поверхность)
         self.image = surf
         # его размер и местоположение располагаем
         # для падения посередине координаты x, а по y будет равно 0
         self.rect = self.image.get_rect(center=(x, 0))
-        self.speed = speed
+        self.speed = speed  # скорость
+        self.score = score  # очки
         self.add(group)
 
     def update(self, *args):
@@ -21,4 +22,3 @@ class Ball(pygame.sprite.Sprite):
             self.rect.y += self.speed
         else:
             self.kill()
-
